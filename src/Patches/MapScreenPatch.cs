525aca538c2057b637912d32725988ac800d9413
B0TLANNER <b0tlanner.games@gmail.com>
2022-10-31 21:53:25 +0200

    Suppport for resuming in fast forward after pausing and moving in fast forward after idling in fast forward

---
+++ src/Patches/MapScreenPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+
+using SandBox.View.Map;
+
+using TaleWorlds.CampaignSystem;
+
+namespace Pacemaker.Patches
+{
+    // Suppport for resuming in fast forward after pausing
+    [HarmonyPatch(typeof(MapScreen), "HandleMouse")]
+    internal static class MapScreenPatch
+    {
+        private static void Postfix(CampaignTimeControlMode __state)
+        {
+            if (__state == CampaignTimeControlMode.StoppableFastForward)
+            {
+                Campaign.Current.TimeControlMode = CampaignTimeControlMode.StoppableFastForward;
+            } 
+            else if (__state == CampaignTimeControlMode.UnstoppableFastForward && Campaign.Current != null && Campaign.Current.TimeControlMode == CampaignTimeControlMode.StoppablePlay)
+            {
+                Campaign.Current.TimeControlMode = CampaignTimeControlMode.StoppableFastForward;
+            }
+        }
+
+        private static void Prefix(ref CampaignTimeControlMode __state)
+        {
+            __state = Campaign.Current != null ? Campaign.Current.TimeControlMode : CampaignTimeControlMode.Stop;
+        }
+    }
+}
